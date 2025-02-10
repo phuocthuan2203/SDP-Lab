a57b459db11bf86c0fb4671e9158edb37eb48d0b
Nguyen Phuoc Thuan <nguyenphuocthuan.work@gmail.com>
2025-02-10 21:31:59 +0700

    initial commit

---
+++ Singleton/NaiveSingleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton.NonThreadSafe
+{
+    public sealed class Singleton {
+        private Singleton() { }
+
+        private static Singleton? _instance;
+
+        public static Singleton GetInstance() {
+            if(_instance == null) {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            Console.WriteLine(s1 == s2);
+        }
+    }
+}
